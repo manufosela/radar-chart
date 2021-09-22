17d6fed6b7f5b3a18cecfc0a34b295758d3fba26
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-09-22 12:53:36 +0200

    show radar chart

--- src/RadarChart.js
+++ src/RadarChart.js
@@ -1,5 +1,5 @@
 import { html, css, LitElement } from 'lit';
-import { Chart } from '../Chart';
+import Chart from 'chart.js/auto'
 
 export class RadarChart extends LitElement {
   static get styles() {
@@ -14,34 +14,113 @@ export class RadarChart extends LitElement {
 
   static get properties() {
     return {
-      height: { type: Number },
+      sideSize: { type: Number, reflect: true, attribute: 'side-size' },
+      labels: { type: Array },
+      marksData: { type: Array },
+      chartOptionsName: { type: String, reflect: true, attribute: 'chart-options-name' },
+      chartOptionsArr: { type: Object, reflect: true }
     };
   }
 
   constructor() {
     super();
-    this.height = 150;
+    this.sideSize = 500;
     this.chartOptionsName = 'default';
     this.chartOptionsArr = { 
-      default: {
-        
+      default: {},
+      dots: {
+        scale: {
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            max: 100,
+            stepSize: 10
+          },
+          pointLabels: {
+            fontSize: 10
+          }
+        }
       }
     };
+    this.marksData = {};
+    this.labelsName = [];
   }
 
-  firstUpdated() {
+  connectedCallback() {
+    super.connectedCallback();
+    this.marksData = {};
+    this.labelsName = [];
+
+    this._setLabels();
+    this._setDataset();
+  }
+
+  setData(marksData, labelName = null) {
+    let index = 0;
+    if (labelName !== null) {
+      index = this.labelsName.indexOf(labelName);
+    }
+    this.marksData.datasets[index].data = marksData;
+    this._drawChart();
+  }
+
+  _setLabels() {
+    const domLabels = [...this.querySelectorAll('.radar-chart__labels li')];
+    this.labels = [];
+    domLabels.forEach((label) => {
+      const labelText = label.innerText;
+      this.labels.push(labelText);
+    });
+    this.marksData.labels = this.labels;
+  }
+
+  _setDataset() {
+    this.marksData.datasets = [];
+    const domMarks = [...this.querySelectorAll('.radar-chart__marks li')];
+    domMarks.forEach((mark) => {
+      const randomRed = Math.floor(Math.random() * 255);
+      const randomGreen = Math.floor(Math.random() * 255);
+      const randomBlue = Math.floor(Math.random() * 255);
+      const markData = {}
+      markData.data = mark.innerText.split(',').map(Number);
+      markData.label = mark.getAttribute('name');
+      this.labelsName.push(mark.getAttribute('name'));
+      markData.backgroundColor = `rgba(${randomRed},${randomGreen},${randomBlue},0.2)`;
+      if (this.chartOptionsName === 'dots') {
+        markData.fill = false;
+        markData.radius = 10;
+        markData.pointRadius = 10;
+        markData.pointBorderWidth = 2;
+        markData.pointBackgroundColor = `rgba(${randomRed},${randomGreen},${randomBlue}, 0.2)`;
+        markData.pointBorderColor = `rgba(${randomRed},${randomGreen},${randomBlue}, 0.6)`;
+        markData.pointHoverRadius = 20;
+      }
+      this.marksData.datasets.push(markData);
+    });
+  }
+
+  _drawChart() {
     this.marksCanvas = this.shadowRoot.querySelector('#marksChart');
-    this.chartOptions = this.chartOptionsArr[this.chartOptionsName]
-    this.radarChart2 = new Chart(this.marksCanvas, {
+    this.chartOptions = this.chartOptionsArr[this.chartOptionsName];
+    if (this.radarChart) {
+      this.radarChart.destroy();
+    }
+    this.radarChart = new Chart(this.marksCanvas, {
       type: 'radar',
       data: this.marksData,
       options: this.chartOptions
     });
   }
 
+  firstUpdated() {
+    this._drawChart();
+  }
+
   render() {
     return html`
-      <canvas id="marksChart" width="600" height="${this.height}"></canvas>
+      <div style="width:${this.sideSize}px; height:${this.sideSize}px;">
+        <canvas id="marksChart"></canvas>
+      </div>
     `;
   }
 }
